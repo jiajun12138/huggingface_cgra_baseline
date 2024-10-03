7c40fc8696dced08cba74e0003e29fb908d1888e
hobbitqia <861394755@qq.com>
2024-10-02 22:56:03 -0500

    debug

--- src/transformers/cgra_op.py
+++ src/transformers/cgra_op.py
@@ -348,16 +348,16 @@ def custom_int_silu(x, bw, term):
     if exp_x[exp_x < 0.0].any():
         print('exp', exp_x.max(), exp_x.min(), exp_x.abs().min())
 
-    if scale.abs() > 2 ** 9:
-        exp_x = exp_x * (scale / 2 ** 9)
-        scale = torch.tensor(2 ** 9)
-
+    if scale.abs() > 2 ** 8:
+        exp_x = exp_x * (scale / 2 ** 8)
+        scale = torch.tensor(2 ** 8)
+    
     exp_plus1 = frac_add(exp_x, torch.tensor(1.0) / scale, bw)
 
-    # if exp_plus1[exp_plus1 <= 1.0].any():
-    #     print('exp_plus', exp_plus1.max(), exp_plus1.min(), exp_plus1.abs().min())
+    if exp_plus1[exp_plus1 <= 1.0 / scale].any():
+        print('exp_plus', exp_plus1.max(), exp_plus1.min(), exp_plus1.abs().min())
     
-    exp_plus1[exp_plus1 <= 1.0] = 1.0
+    exp_plus1[exp_plus1 <= 1.0 / scale] = 1.0 / scale
 
     ans = (frac_div(fp_x / o_scale, exp_plus1, bw) * o_scale / scale).to(x.dtype)
 
